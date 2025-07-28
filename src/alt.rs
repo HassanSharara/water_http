c6a16df5d6b31d17d2b1251faccb3963dabbc198
pc <ft47fc@gmail.com>
2025-07-28 12:37:42 +0300

    adding water_http_utils package

---
+++ src/alt.rs
@@ -0,0 +1,63 @@
+use water_http::server::{ServerConfigurations};
+use water_http::{InitControllersRoot, WaterController};
+
+
+InitControllersRoot! {
+    name:MAIN_ROOT,
+    holder_type:MainHolderType,
+}
+type MainHolderType = u8;
+
+
+#[tokio::main]
+async fn main() {
+
+    // when debugging feature enabled
+    #[cfg(feature = "debugging")]
+    {
+        let subscriber  = tracing_subscriber::FmtSubscriber::builder()
+            .with_max_level(tracing::Level::DEBUG)
+            .finish();
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("no thing");
+    }
+
+
+    let  config = ServerConfigurations::bind("127.0.0.1",8084);
+    water_http::RunServer!(
+        config,
+        MAIN_ROOT,
+        MainController
+    );
+}
+
+WaterController! {
+    holder -> crate::MainHolderType,
+    name -> MainController,
+    functions -> {
+
+        GET => / => main(context) async {}
+    }
+    extra_code->(..{
+
+
+
+    })
+}
+#[derive(serde::Serialize)]
+pub struct Response {
+    status:&'static str,
+    body:&'static str
+}
+impl Response {
+
+    pub fn test()->Self{ Self {status:"success",body:"yes this is response"}}
+}
+
+
+
+
+
+
+
+
