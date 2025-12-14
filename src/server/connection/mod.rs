2bfdc66f81e1a7af354e6e3c6e78700d348025b0
HassanSharara <hassansharara12@gmail.com>
2025-12-14 18:13:29 +0300

    for testing

--- src/server/connection/mod.rs
+++ src/server/connection/mod.rs
@@ -488,7 +488,7 @@ pub (crate) fn reserve_buf(buffer: &mut BytesMut) {
     const MIN_RESERVE: usize = 1024;
     let remaining = buffer.capacity() - buffer.len();
     if remaining < MIN_RESERVE {
-        buffer.reserve(READING_BUF_LEN);
+        buffer.reserve(MIN_RESERVE * 2);
     }
 }
 
