8cf42ff9333555bcf014f68b053d6b76fae77e6a
HassanSharara <hassansharara12@gmail.com>
2026-03-10 01:26:37 +0300

    test

--- src/server/configurations.rs
+++ src/server/configurations.rs
@@ -7,7 +7,7 @@ use crate::server::encoding::{EncodingConfigurations};
 pub (crate) const EACH_REQUEST_BODY_READING_BUFFER:usize = 4096*4;
 // pub (crate) const EACH_REQUEST_BODY_WRITING_BUFFER:usize = 1024*4;
 pub (crate) const READING_BUF_LEN:usize = 4096*4;
-pub (crate) const WRITING_BUF_LEN:usize = 4096*4;
+pub (crate) const WRITING_BUF_LEN:usize = 4096*8;
 pub (crate) const WRITING_FILES_BUF_LEN:usize = 1024*20;
 
 
--- src/server/connection/mod.rs
+++ src/server/connection/mod.rs
@@ -906,8 +906,6 @@ pub (crate) async fn handle_responding<'e>
             todo!()
         }
         HttpStream::Async(h) => {
-            #[cfg(feature = "debugging")]
-            println!("writing {:?}",String::from_utf8_lossy(&response_buf));
 
            let (r,mut b) =  h.write_all(response_buf).await;
             if r.is_err() { return Err("can not write data to given buffer")}
